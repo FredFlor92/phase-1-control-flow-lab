bf2055b91301530a00d9268708842bdd906260e7
Freddy Florial <freddyflorial@gmail.com>
2022-07-19 20:52:27 -0400

    commit tree d188611b17c4e3cf34f3ddbc11cfa73813fd06cd

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400){
+    return 'This one is on me!';
+  }else if (someValue > 400 && someValue < 2000){
+    return 'That will be twenty bucks.';
+  }else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if (someValue > 2500){
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if(city == 'NYC'){
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.' ;
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+    }
   // Write your code here!
 }
